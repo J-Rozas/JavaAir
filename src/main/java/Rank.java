5a23b0d7faaf616c7706c3d9eb884c5bfd19dd69
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-17 17:29:02 +0100

    Refactor CabinCrewMember into Rank

---
+++ src/main/java/Rank.java
@@ -0,0 +1,16 @@
+public enum Rank {
+    PURSER("Purser"),
+    FLIGHT_ATTENDANT("Flight attendant"),
+    FIRST_OFFICER("First Officer"),
+    CAPTAIN("Captain");
+
+    private final String rankName;
+
+    Rank(String rankName) {
+        this.rankName = rankName;
+    }
+
+    public String getRankName() {
+        return this.rankName;
+    }
+}

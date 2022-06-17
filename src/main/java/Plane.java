086abceda1d160de99eeea53e9ffa2b6da8574e3
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-17 17:07:32 +0100

    Add class Plane

---
+++ src/main/java/Plane.java
@@ -0,0 +1,12 @@
+public class Plane {
+
+    private final PlaneType type;
+
+    public Plane(PlaneType type) {
+        this.type = type;
+    }
+
+    public PlaneType getType() {
+        return this.type;
+    }
+}

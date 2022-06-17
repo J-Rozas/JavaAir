0ebf132b95b5dfcdcf892bce3bc7eaa636deb50c
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-17 17:06:54 +0100

    Add PlaneType Enum

---
+++ src/main/java/PlaneType.java
@@ -0,0 +1,11 @@
+public enum PlaneType {
+    BOEING747(300, 10000000);
+
+    private final int capacity;
+    private final int weight;
+
+    PlaneType(int capacity, int weight) {
+        this.capacity = capacity;
+        this.weight = weight;
+    }
+}

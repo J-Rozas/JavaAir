417da8d55ee8a8989e4af912b22c0d410a72b710
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-17 16:03:18 +0100

    Add tests for class Passenger

---
+++ src/test/java/PassengerTest.java
@@ -0,0 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PassengerTest {
+
+    private Passenger passenger;
+
+    @Before
+    public void before() {
+        passenger = new Passenger("John", 3);
+    }
+
+    @Test
+    public void passengerHasName() {
+        assertEquals("John", passenger.getName());
+    }
+
+    @Test
+    public void passengerHasNumberOfBags() {
+        assertEquals("3", passenger.getNumberOfbags());
+    }
+}

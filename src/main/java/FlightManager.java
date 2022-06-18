1fa4639c2bfb5481de0110c484ea6e8f6059eee4
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-18 17:41:11 +0100

    Add method to calculate booked weight in a flight

--- src/main/java/FlightManager.java
+++ src/main/java/FlightManager.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class FlightManager extends Person {
 
     public FlightManager(String name) {
@@ -8,5 +10,17 @@ public class FlightManager extends Person {
         int totalWeightForBaggage = flight.getPlane().getType().getWeight() / 2;
         int totalCapacity = flight.getPlane().getType().getCapacity();
         return totalWeightForBaggage / totalCapacity;
+    }
+
+    public int calculatedBookedWeight(Flight flight) {
+        ArrayList<Passenger> passengers = flight.getPassengers();
+        int totalWeight = 0;
+        //        NOTE: We are assuming that all the bags weight the same, i.e., 20
+
+        for (Passenger passenger : passengers) {
+            totalWeight += passenger.getNumberOfBags() * 20;
+        }
+
+        return totalWeight;
     }
 }

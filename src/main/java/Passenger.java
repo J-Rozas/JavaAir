5de425d5154628122544866198e4c971fe11718d
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-19 17:56:12 +0100

    Add flightStatus attribute to passenger and Setter and Getter

--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -1,13 +1,23 @@
 public class Passenger extends Person {
 
     private final int numberOfBags;
+    private boolean flightStatus;
 
     public Passenger(String name, int numberOfBags) {
         super(name);
         this.numberOfBags = numberOfBags;
+        this.flightStatus = false;
     }
 
     public int getNumberOfBags() {
         return this.numberOfBags;
+    }
+
+    public boolean getFlightStatus() {
+        return this.flightStatus;
+    }
+
+    public void setFlightStatus(boolean status) {
+        this.flightStatus = status;
     }
 }

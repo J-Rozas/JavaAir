eca5224347ce0ced4b194867f8571c579d45bdfa
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-19 19:26:22 +0100

    Add seatNumber property to passenger and Setter and Getter

--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -2,11 +2,13 @@ public class Passenger extends Person {
 
     private final int numberOfBags;
     private boolean flightStatus;
+    private int seatNumber;
 
     public Passenger(String name, int numberOfBags) {
         super(name);
         this.numberOfBags = numberOfBags;
         this.flightStatus = false;
+        this.seatNumber = 0;
     }
 
     public int getNumberOfBags() {
@@ -19,5 +21,13 @@ public class Passenger extends Person {
 
     public void setFlightStatus(boolean status) {
         this.flightStatus = status;
+    }
+
+    public int getSeatNumber() {
+        return this.seatNumber;
+    }
+
+    public void setSeatNumber(int seatNumber) {
+        this.seatNumber = seatNumber;
     }
 }
